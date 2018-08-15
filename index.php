63863167760454a0833c1971fd8eac120db715c0
GeorgeSteel <julianacero98@gmail.com>
2018-08-15 18:14:32 -0500

    versionFinal

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
     <meta charset="utf-8">
     <title>Index de imagenes</title>
   </head>
-  <h1>Curso git vergas</h1>
-  <h2>Conexion con GitHub</h2>
-  <p>Git está rotísimo</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <body>
+    <body>
     <form action="proceso_guardar.php" method="post" enctype="multipart/form-data">
       <input type="text" name="nombre"  placeholder="Nombre" value="" required>
       <br><br>
@@ -17,7 +13,5 @@
       <input type="submit" name="" value="Aceptar">
     </form>
   </body>
-  <footer>
-    <h1>laiudhfvgpaiwuhgvijaSNcd</h1>
-  </footer>
+  
 </html>
